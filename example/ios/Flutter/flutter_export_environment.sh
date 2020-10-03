b36ae8e1c34b9bbb2d8b8f158ccbfcc9d1f70afc
DESKTOP-F6G4959\BMA <bader.aalami@gmail.com>
2020-10-03 23:37:08 +0300

    first commit

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=F:\FlutterDev\flutter"
-export "FLUTTER_APPLICATION_PATH=F:\Flutter_SandBox\flick-video-player\example"
+export "FLUTTER_APPLICATION_PATH=F:\Flutter_SandBox\Packages\flick-video-player-cached\example"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
--- lib/flick_video_player.dart
+++ lib/flick_video_player.dart
@@ -30,3 +30,5 @@ export './src/widgets/helpers/auto_play_timer_progress/auto_play_timer_progress.
 export './src/controls/flick_video_with_controls.dart';
 export './src/controls/flick_portrait_controls.dart';
 export './src/controls/flick_landscape_controls.dart';
+
+//test

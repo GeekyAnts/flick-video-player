e3d95f468129e263b634c2a0548ae24c8e4bb41b
Ruchika Gupta <ruchika@Ruchikas-MacBook-Pro.local>
2021-03-17 15:33:22 +0530

    upgraded video player to ^2.0.2

--- example/lib/animation_player/animation_player.dart
+++ example/lib/animation_player/animation_player.dart
@@ -2,7 +2,7 @@ import 'package:example/animation_player/portrait_video_controls.dart';
 import 'package:example/utils/mock_data.dart';
 import 'package:flick_video_player/flick_video_player.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_widgets/flutter_widgets.dart';
+import 'package:visibility_detector/visibility_detector.dart';
 import 'package:video_player/video_player.dart';
 
 import './data_manager.dart';

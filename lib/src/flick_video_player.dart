af54a25cd0bea67e083c385a1c0850b0a4935677
Haythem Dridi <haithemdridiweb@gmail.com>
2024-05-01 15:43:57 +0100

    use wakelock_plus instead of wakelock

--- lib/src/flick_video_player.dart
+++ lib/src/flick_video_player.dart
@@ -1,10 +1,10 @@
-import 'package:flick_video_player/src/utils/web_key_bindings.dart';
-import 'package:universal_html/html.dart';
 import 'package:flick_video_player/flick_video_player.dart';
+import 'package:flick_video_player/src/utils/web_key_bindings.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:wakelock/wakelock.dart';
+import 'package:universal_html/html.dart';
+import 'package:wakelock_plus/wakelock_plus.dart';
 
 class FlickVideoPlayer extends StatefulWidget {
   const FlickVideoPlayer({
@@ -86,7 +86,7 @@ class _FlickVideoPlayerState extends State<FlickVideoPlayer>
     _setPreferredOrientation();
 
     if (widget.wakelockEnabled) {
-      Wakelock.enable();
+      WakelockPlus.enable();
     }
 
     if (kIsWeb) {
@@ -102,7 +102,7 @@ class _FlickVideoPlayerState extends State<FlickVideoPlayer>
   void dispose() {
     flickManager.flickControlManager!.removeListener(listener);
     if (widget.wakelockEnabled) {
-      Wakelock.disable();
+      WakelockPlus.disable();
     }
     WidgetsBinding.instance.removeObserver(this);
     super.dispose();
@@ -131,8 +131,8 @@ class _FlickVideoPlayerState extends State<FlickVideoPlayer>
   _switchToFullscreen() {
     if (widget.wakelockEnabledFullscreen) {
       /// Disable previous wakelock setting.
-      Wakelock.disable();
-      Wakelock.enable();
+      WakelockPlus.disable();
+      WakelockPlus.enable();
     }
 
     _isFullscreen = true;
@@ -163,8 +163,8 @@ class _FlickVideoPlayerState extends State<FlickVideoPlayer>
   _exitFullscreen() {
     if (widget.wakelockEnabled) {
       /// Disable previous wakelock setting.
-      Wakelock.disable();
-      Wakelock.enable();
+      WakelockPlus.disable();
+      WakelockPlus.enable();
     }
 
     _isFullscreen = false;
